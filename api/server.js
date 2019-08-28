77e45761ae9dc4bc66a6821725bc17c78933cb71
Mariana Huh <mhuh0176@gmail.com>
2019-08-27 19:54:41 -0700

    jwt works

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
@@ -8,8 +12,28 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
 server.get('/', (req, res) => {
   res.send("It's alive! Muuhahaaa!");
+});
+
+server.get('/token', (req, res) => {
+  const payload = {
+    subject: 'user',
+    username: 'mhuh',
+    favoriteDay: 'Holiday'
+  };
+  const secret = 'thisisasecret';
+  const options = {
+    expiresIn: '1h'
+  };
+
+  const token = jwt.sign(payload, secret, options);
+  console.log(token);
+
+  res.json(token);
 });
 
 module.exports = server;
